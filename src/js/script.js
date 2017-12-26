07c28ecf3fa156a6f4855a1267b35423efad34ca
nsarka <nsarka00@gmail.com>
2017-12-26 00:53:22 -0500

    Timer for update

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,22 @@
 $(function() {
-	var d;
-	
-	$.ajax({
-		url : '/api/coinflips',
-		type : 'GET',
-		success : function(data) {              
-			d = data;
-			var template = document.getElementById('coinflips-row-template').innerHTML;
-			var renderRows = Handlebars.compile(template);
-			document.getElementById('coinflip-row').innerHTML = renderRows({
-				coinflips: d,
-			});
-		},
-		error : function(request,error)
-		{
-			alert("Request: "+JSON.stringify(request));
-		}
+
+setInterval(function() {
+		$.ajax({
+			url : '/api/coinflips',
+			type : 'GET',
+			success : function(data) {              
+				var d = data;
+				var template = document.getElementById('coinflips-row-template').innerHTML;
+				var renderRows = Handlebars.compile(template);
+				document.getElementById('coinflip-row').innerHTML = renderRows({
+					coinflips: d,
+				});
+			},
+			error : function(request,error)
+			{
+				alert("Request: "+JSON.stringify(request));
+			}
+		});
 	});
-});
+}, 2000);
+
