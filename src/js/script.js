f704a508ff8b477deeeea830b96d55ead41a45c2
nsarka <nsarka00@gmail.com>
2017-12-26 00:28:57 -0500

    Can POST to make flips, AJAX on client to get and view data

--- src/js/script.js
+++ src/js/script.js
@@ -1,170 +1,20 @@
 $(function() {
-	var data = [
-		{
-			p1img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48df68e361072c39abe04f74af20459d0b59c104_full.jpg',
-			p2img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e2/e289522f35b2867ea202e382543c51856122537a_full.jpg',
-			items: [
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/2425581552/86fx57f',
-					name: 'AK-47 | Red Laminate (Field-Tested) - 8.27'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/992512702/86fx57f',
-					name: 'M4A1-S | Hot Rod (Minimal Wear) - 70.06'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/1011935077/86fx57f',
-					name: 'AWP | Hyper Beast (Field-Tested) - 25.04'
-				}
-			],
-			value: 100.0,
-			status: 1
+	var d;
+	
+	$.ajax({
+		url : '/api/coinflips',
+		type : 'GET',
+		success : function(data) {              
+			d = data;
+			var template = document.getElementById('coinflips-row-template').innerHTML;
+			var renderRows = Handlebars.compile(template);
+			document.getElementById('coinflip-row').innerHTML = renderRows({
+				coinflips: d,
+			});
 		},
+		error : function(request,error)
 		{
-			p1img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48df68e361072c39abe04f74af20459d0b59c104_full.jpg',
-			p2img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e2/e289522f35b2867ea202e382543c51856122537a_full.jpg',
-			items: [
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/2425581552/86fx57f',
-					name: 'AK-47 | Red Laminate (Field-Tested) - 8.27'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/992512702/86fx57f',
-					name: 'M4A1-S | Hot Rod (Minimal Wear) - 70.06'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/1011935077/86fx57f',
-					name: 'AWP | Hyper Beast (Field-Tested) - 25.04'
-				}
-			],
-			value: 100.0,
-			status: 0
-		},
-		{
-			p1img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48df68e361072c39abe04f74af20459d0b59c104_full.jpg',
-			p2img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e2/e289522f35b2867ea202e382543c51856122537a_full.jpg',
-			items: [
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/2425581552/86fx57f',
-					name: 'AK-47 | Red Laminate (Field-Tested) - 8.27'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/992512702/86fx57f',
-					name: 'M4A1-S | Hot Rod (Minimal Wear) - 70.06'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/1011935077/86fx57f',
-					name: 'AWP | Hyper Beast (Field-Tested) - 25.04'
-				}
-			],
-			value: 100.0,
-			status: 0
-		},
-		{
-			p1img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48df68e361072c39abe04f74af20459d0b59c104_full.jpg',
-			p2img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e2/e289522f35b2867ea202e382543c51856122537a_full.jpg',
-			items: [
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/2425581552/86fx57f',
-					name: 'AK-47 | Red Laminate (Field-Tested) - 8.27'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/992512702/86fx57f',
-					name: 'M4A1-S | Hot Rod (Minimal Wear) - 70.06'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/1011935077/86fx57f',
-					name: 'AWP | Hyper Beast (Field-Tested) - 25.04'
-				}
-			],
-			value: 100.0,
-			status: 1
-		},
-		{
-			p1img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48df68e361072c39abe04f74af20459d0b59c104_full.jpg',
-			p2img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e2/e289522f35b2867ea202e382543c51856122537a_full.jpg',
-			items: [
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/2425581552/86fx57f',
-					name: 'AK-47 | Red Laminate (Field-Tested) - 8.27'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/992512702/86fx57f',
-					name: 'M4A1-S | Hot Rod (Minimal Wear) - 70.06'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/1011935077/86fx57f',
-					name: 'AWP | Hyper Beast (Field-Tested) - 25.04'
-				}
-			],
-			value: 100.0,
-			status: 0
-		},
-		{
-			p1img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48df68e361072c39abe04f74af20459d0b59c104_full.jpg',
-			p2img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e2/e289522f35b2867ea202e382543c51856122537a_full.jpg',
-			items: [
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/2425581552/86fx57f',
-					name: 'AK-47 | Red Laminate (Field-Tested) - 8.27'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/992512702/86fx57f',
-					name: 'M4A1-S | Hot Rod (Minimal Wear) - 70.06'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/1011935077/86fx57f',
-					name: 'AWP | Hyper Beast (Field-Tested) - 25.04'
-				}
-			],
-			value: 100.0,
-			status: 1
-		},
-		{
-			p1img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48df68e361072c39abe04f74af20459d0b59c104_full.jpg',
-			p2img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e2/e289522f35b2867ea202e382543c51856122537a_full.jpg',
-			items: [
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/2425581552/86fx57f',
-					name: 'AK-47 | Red Laminate (Field-Tested) - 8.27'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/992512702/86fx57f',
-					name: 'M4A1-S | Hot Rod (Minimal Wear) - 70.06'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/1011935077/86fx57f',
-					name: 'AWP | Hyper Beast (Field-Tested) - 25.04'
-				}
-			],
-			value: 100.0,
-			status: 1
-		},
-		{
-			p1img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/48/48df68e361072c39abe04f74af20459d0b59c104_full.jpg',
-			p2img: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/e2/e289522f35b2867ea202e382543c51856122537a_full.jpg',
-			items: [
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/2425581552/86fx57f',
-					name: 'AK-47 | Red Laminate (Field-Tested) - 8.27'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/992512702/86fx57f',
-					name: 'M4A1-S | Hot Rod (Minimal Wear) - 70.06'
-				},
-				{
-					img: 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/1011935077/86fx57f',
-					name: 'AWP | Hyper Beast (Field-Tested) - 25.04'
-				}
-			],
-			value: 100.0,
-			status: 0
+			alert("Request: "+JSON.stringify(request));
 		}
-	]
-
-	var template = document.getElementById('coinflips-row-template').innerHTML;
-	var renderRows = Handlebars.compile(template);
-	document.getElementById('coinflip-row').innerHTML = renderRows({
-		coinflips: data,
 	});
 });
